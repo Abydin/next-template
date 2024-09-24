79c6648dd008d640e8bb13b77cde0396789b5476
Abydin <abideenayinde7@gmail.com>
2024-09-24 12:33:43 +0100

    fix(eslint): updated eslintrs.js error

--- src/tests/Button.test.tsx
+++ src/tests/Button.test.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 
-import '@testing-library/jest-dom';
-
 import { Button } from '../components/atoms/Button';
 
 describe('Button Component', () => {
