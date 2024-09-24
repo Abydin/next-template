c4e89c98732226cc1e89d2ed776b9e2f068f739c
Abydin <abideenayinde7@gmail.com>
2024-09-24 07:41:42 +0100

    fix(action): update project to switch from babel to swr

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,25 @@
 module.exports = {
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.jest.json',
+    },
+  },
+
   moduleDirectories: ['node_modules', 'src'],
 
   // Optional Jest setup file
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
+
   preset: 'ts-jest',
 
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-
   testEnvironment: 'jest-environment-jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   transform: {
-    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+    '^.+\\.(js|jsx|ts|tsx)$': '@swc/jest', // Use @swc/jest for transforming files
   },
-  transformIgnorePatterns: [
-    '/node_modules/(?!@babel/runtime)', // Allow specific ES6 modules to be transformed
-  ],
+  transformIgnorePatterns: ['/node_modules/'],
 };
